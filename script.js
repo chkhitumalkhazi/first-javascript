c586e8d0353ef6b6a5bc44ca93f62d04b8be684c
Ckhitu Malkhaz <60267266+chkhitumalkhazi@users.noreply.github.com>
2023-07-23 11:37:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var me = ('me var')
+let maxo = (' maxo')
+const kai = (' me rare zaodeli var 😎')
+const num1 = '2';
+const num2 = '3';
+console.log(me)
+console.log(maxo)
+console.log(maxo+kai)
+console.log(num2+num1);
+document.write(me+maxo+kai);
+document.write(num2+num1);
